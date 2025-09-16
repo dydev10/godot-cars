add5188ac2dfbc7ec66a2c7c9483d9db6b5c1f55
DY Dev <dydev420@gmail.com>
2025-09-16 07:13:23 +0530

    refactor: move wheel physics logic to wheel class

--- raycast_car.gd
+++ raycast_car.gd
@@ -1,4 +1,5 @@
 extends RigidBody3D
+class_name RaycastCar
 
 @export var wheels: Array[RaycastWheel]
 @export var acceleration := 600.0
@@ -10,9 +11,10 @@ extends RigidBody3D
 @export var skidMarks: Array[GPUParticles3D]
 
 # debug state
-@export var debugArrow := true
-@export var debugPoint := true
+@export var showDebug := false
 
+
+@onready var totalWheels := wheels.size()
 
 var motor_input := 0
 var handbrake := false
@@ -38,18 +40,27 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	_basic_steering_rotation(delta)
-	
 	var isGrounded := false
 	var id := 0
 	
 	for wheel in wheels:
+		_basic_steering_rotation(wheel, delta)
+		wheel._apply_wheel_physics(self)
+		
+		# skid marks when drifitng or slipping
+		skidMarks[id].global_position = wheel.get_collision_point() + Vector3.UP * 0.01
+		skidMarks[id].look_at(skidMarks[id].global_position + global_basis.z)
+		
+		if not handbrake and wheel.gripFactor < 0.2:
+			isSlipping = false
+			skidMarks[id].emitting = false
+	
+		if handbrake and not skidMarks[id].emitting:
+			skidMarks[id].emitting = true
+		
 		if wheel.is_colliding():
 			isGrounded = true
-		wheel.force_raycast_update()
-		_do_single_wheel_suspension(wheel)
-		_do_single_wheel_acceleration(wheel)
-		_do_single_wheel_traction(wheel, id)
+		
 		id += 1
 	
 	if isGrounded:
@@ -58,118 +69,20 @@ func _physics_process(delta: float) -> void:
 		center_of_mass_mode = RigidBody3D.CENTER_OF_MASS_MODE_CUSTOM
 		center_of_mass = Vector3.DOWN * 0.5
 		
-	if debugPoint:
+	if showDebug:
 		DebugDraw3D.draw_sphere(global_position + center_of_mass, 0.2)
-	if debugArrow:
-		DebugDraw3D.draw_arrow_ray(global_position, linear_velocity, 1.0, Color.YELLOW, 0.02)
+		DebugDraw3D.draw_arrow_ray(global_position, linear_velocity, 1.0, Color.GREEN, 0.02)
 
 
 func _get_point_velocity(point: Vector3) -> Vector3:
 	return linear_velocity + angular_velocity.cross(point - global_position)  # TODO: study this forumla to get linear velocity of a point on rigid body
 
 
-func _basic_steering_rotation(delta) -> void:
+func _basic_steering_rotation(wheel: RaycastWheel, delta: float) -> void:
+	if not wheel.isSteer: return
+	
 	var turnInput := Input.get_axis("move_right", "move_left") * tireTurnSpeed
-	
 	if turnInput:
-		$WheelFL.rotation.y = clamp($WheelFL.rotation.y + turnInput * delta, -deg_to_rad(tireMaxTurnDegree), deg_to_rad(tireMaxTurnDegree))
-		$WheelFR.rotation.y = clamp($WheelFR.rotation.y + turnInput * delta, -deg_to_rad(tireMaxTurnDegree), deg_to_rad(tireMaxTurnDegree))
+		wheel.rotation.y = clamp(wheel.rotation.y + turnInput * delta, -deg_to_rad(tireMaxTurnDegree), deg_to_rad(tireMaxTurnDegree))
 	else:
-		$WheelFL.rotation.y = move_toward($WheelFL.rotation.y, 0.0, tireTurnSpeed * delta)
-		$WheelFR.rotation.y = move_toward($WheelFR.rotation.y, 0.0, tireTurnSpeed * delta)
-
-
-func _do_single_wheel_traction(ray: RaycastWheel, idx: int) -> void:
-	if not ray.is_colliding(): return
-	
-	var steerSideDir := ray.global_basis.x
-	var tireVel := _get_point_velocity(ray.wheel.global_position)
-	var xSteeringVel := steerSideDir.dot(tireVel)
-	
-	var gripFactor := absf(xSteeringVel / tireVel.length())
-	var xTraction := ray.gripCurve.sample_baked(gripFactor)
-	
-	# skid marks when drifitng or slipping
-	skidMarks[idx].global_position = ray.get_collision_point() + Vector3.UP * 0.01
-	skidMarks[idx].look_at(skidMarks[idx].global_position + global_basis.z)
-	
-	if not handbrake and gripFactor < 0.2:
-		isSlipping = false
-		skidMarks[idx].emitting = false
-	
-	# drift by nerfing xTraction when handbrake
-	if handbrake:
-		xTraction = 0.01
-		if not skidMarks[idx].emitting:
-			skidMarks[idx].emitting = true
-	elif isSlipping:
-		xTraction = 0.1
-	
-	
-	## F = M * dV/T
-	#var desiredAccel := (xSteeringVel * xTraction) / get_process_delta_time()
-	#var xForce := -steerSideDir * desiredAccel * (mass / 4.0)  # TODO: use spring force to calculate load on each tire instead of mass/4
-	
-	var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-	var xForce := -steerSideDir * xSteeringVel * xTraction * ((mass * gravity) / 4.0)  # TODO: use spring force to calculate load on each tire instead of mass/4
-	
-	var fVel := -ray.global_basis.z.dot(tireVel)
-	var zTraction := 0.05
-	#var zForce := ray.global_basis.z * fVel * zTraction * ((mass * gravity) / 4.0)
-	var zForce := global_basis.z * fVel * zTraction * ((mass * gravity) / 4.0)
-	
-	var forcePos := ray.wheel.global_position - global_position
-	apply_force(xForce, forcePos)
-	apply_force(zForce, forcePos)
-	if debugArrow:
-		DebugDraw3D.draw_arrow_ray(ray.wheel.global_position, xForce/mass, 1.0, Color.GREEN, 0.02)
-		DebugDraw3D.draw_arrow_ray(ray.wheel.global_position, zForce/mass, 1.0, Color.PINK, 0.02)
-
-
-func _do_single_wheel_acceleration(ray: RaycastWheel) -> void:
-	var forwardDir := -ray.global_basis.z
-	var vel := forwardDir.dot(linear_velocity)	
-	ray.wheel.rotate_x((-vel * get_process_delta_time()) / ray.wheelRadius)
-	
-	if ray.is_colliding():
-		var contact := ray.wheel.global_position
-		var forcePos := contact - global_position
-			
-		if ray.isMotor and motor_input:
-			var speedRatio := vel / maxSpeed
-			var power = powerCurve.sample_baked(speedRatio)
-			var forceVector: Vector3 = forwardDir * acceleration * motor_input * power
-			apply_force(forceVector, forcePos)
-			if debugArrow:
-				DebugDraw3D.draw_arrow_ray(contact, forceVector/mass, 1.0, Color.RED, 0.02)
-
-
-func _do_single_wheel_suspension(ray: RaycastWheel) -> void:
-	if ray.is_colliding():
-		# limit the raycast target distance
-		ray.target_position.y = -(ray.restDist + ray.wheelRadius + ray.overExtend)
-		
-		# calculate state of spring
-		var contact := ray.get_collision_point()
-		var springUpDir := ray.global_transform.basis.y
-		var springLen := ray.global_position.distance_to(contact) - ray.wheelRadius
-		var offset := ray.restDist - springLen
-		
-		# adjust wheel mesh based on spring length
-		ray.wheel.position.y = -springLen
-		
-		var springForce := ray.springStrengh * offset
-		
-		var worldVel := _get_point_velocity(contact)
-		var relativeVel := springUpDir.dot(worldVel)
-		var springDampForce := ray.springDamping * relativeVel
-		
-		var forceVector := (springForce - springDampForce) * ray.get_collision_normal()
-		
-		var forcePosOffset = ray.wheel.global_position - global_position
-		apply_force(forceVector, forcePosOffset)
-
-
-		# debug things
-		if debugArrow:
-			DebugDraw3D.draw_arrow_ray(ray.wheel.global_position, forceVector/mass, 1.0, Color.BLUE, 0.02);
+		wheel.rotation.y = move_toward(wheel.rotation.y, 0.0, tireTurnSpeed * delta)
--- raycast_wheel.gd
+++ raycast_wheel.gd
@@ -1,12 +1,128 @@
 extends RayCast3D
 class_name RaycastWheel
 
+@export_group("Suspension")
 @export var springStrengh := 100
 @export var springDamping := 2.0  # damp coefficient is decided by: d=z(2Sqrt[k*m]) eg: for z=0.1,m=50,k=5000, d-> 31.6228
 @export var restDist := 0.5
 @export var wheelRadius := 0.4
 @export var overExtend := 0.0
+
+@export_group("Motor & Traction")
 @export var isMotor := false
+@export var isSteer := false
 @export var gripCurve: Curve
 
+@export_group("Debug Wheel")
+@export var showDebug := false
+
 @onready var wheel: Node3D = get_child(0)  # assumes mesh is the first child node
+
+# global var so that it can be used by RaycastCar class
+var gripFactor := 0.0
+
+func _ready() -> void:
+	# set the raycast target to account for wheel size and allowed over extension 
+	target_position.y = -(restDist + wheelRadius + overExtend)
+
+
+func _apply_wheel_physics(car: RaycastCar) -> void:
+	# need to force raycast to update before other calculations in this physics step
+	force_raycast_update()
+	
+	## Get forward fdirection and velocity of wheel/raycast 
+	var forwardDir := -global_basis.z
+	var vel := forwardDir.dot(car.linear_velocity)
+	
+	## Rotate wheel mesh
+	wheel.rotate_x((-vel * get_process_delta_time()) / wheelRadius)
+	
+	# no forces added by this wheel if it is not on ground
+	if not is_colliding(): return
+	
+	# wheel is on the ground, calculate all forces and apply on car body
+	
+	## Calculate the spring length based on raycast hit distance
+	var rayHit := get_collision_point()
+	var springLen := global_position.distance_to(rayHit) - wheelRadius
+	var offset := restDist - springLen
+	
+	## Adjust local y position of wheel mesh based on spring length
+	wheel.position.y = -springLen
+
+	## Set contact point as the wheel origin point and calculate local force position on car body
+	var contact := wheel.global_position
+	var forcePos := contact - car.global_position
+	
+	##
+	#
+	## Spring forces
+	var springForce := springStrengh * offset
+	var tireVel := car._get_point_velocity(contact)
+	var springUpDir := global_basis.y
+	var springVel := springUpDir.dot(tireVel)
+	var springDampForce := springDamping * springVel
+	
+	## Final suspension force (yForce)
+	var yForce: Vector3 = (springForce - springDampForce) * get_collision_normal()
+	
+	##
+	#
+	## Acceleration
+	var accelForce := Vector3.ZERO
+	if isMotor and car.motor_input:
+		var speedRatio := vel / car.maxSpeed
+		var power = car.powerCurve.sample_baked(speedRatio)
+		accelForce = forwardDir * car.acceleration * car.motor_input * power
+	
+	##
+	#
+	## Tire X Traction (Steering)
+	var steerSideDir := global_basis.x
+	var xSteeringVel := steerSideDir.dot(tireVel)
+	
+	## Get dynamic tire grip from curve based on slip ratio
+	gripFactor = absf(xSteeringVel / tireVel.length())
+	var xTraction := gripCurve.sample_baked(gripFactor)
+
+	## Stop drifting if tire has enough grip and no handbrake input
+	if not car.handbrake and gripFactor < 0.2:
+		car.isSlipping = false
+	
+	## Drift by nerfing xTraction when handbrake or keep drifting if in drift state but with increased traction when no handbrake
+	if car.handbrake:
+		xTraction = 0.01
+	elif car.isSlipping:
+		xTraction = 0.1
+	
+	## Get final gravity acting on car body
+	var gravity = -car.get_gravity().y
+	
+	## Final Steer force (xForce)
+	var xForce: Vector3 = -steerSideDir * xSteeringVel * xTraction * ((car.mass * gravity) / car.totalWheels)  # TODO: use spring force to calculate load on each tire instead of mass/totalWheels
+
+	##
+	#
+	## Tire Z Traction (Drag)
+	var fVel := forwardDir.dot(tireVel)
+	var zTraction := 0.05
+	
+	## Final tire drag force (zForce)
+	var zForce: Vector3 = global_basis.z * fVel * zTraction * ((car.mass * gravity) / car.totalWheels)
+	
+	##
+	#
+	## Apply forces to car body
+	car.apply_force(yForce, forcePos)      # suspension
+	car.apply_force(xForce, forcePos)      # steer
+	car.apply_force(zForce, forcePos)      # drag
+	car.apply_force(accelForce, forcePos)  #acceleration
+	
+	##
+	#
+	## Debug Force Arrows
+	if showDebug:
+		DebugDraw3D.draw_arrow_ray(contact, yForce/car.mass, 1.0, Color.BLUE, 0.02)
+		DebugDraw3D.draw_arrow_ray(contact, xForce/car.mass, 1.0, Color.YELLOW, 0.02)
+		DebugDraw3D.draw_arrow_ray(contact, zForce/car.mass, 1.0, Color.PINK, 0.02)
+		DebugDraw3D.draw_arrow_ray(contact, accelForce/car.mass, 1.0, Color.RED, 0.02)
